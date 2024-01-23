aeb6cd3de4a99fe93373623c3a1b6e159459aae0
Robby Morgan <robby.d.morgan@gmail.com>
2024-01-23 16:40:35 -0600

    cleaned up forecast section

--- src/Components/Forecast.js
+++ src/Components/Forecast.js
@@ -6,15 +6,17 @@ function Forecast({forecastData}) {
    
 
     return (
-        <ul>
-            {forecastList && forecastList.length > 0 && forecastList
-      .filter((item, index, self) => self.findIndex((i) => new Date(i.dt * 1000).toDateString() === new Date(item.dt * 1000).toDateString()) === index)
-      .map((item) => (
-        <li key={item.dt}>
-          {new Date(item.dt * 1000).toLocaleDateString()} <span>{item.main.humidity} {Math.floor(item.main.temp_max)}&deg; {Math.floor(item.main.temp_min)}&deg;</span>
-        </li>
-      ))}
-        </ul>
+        <div className="forecast-container">
+            <ul>
+                {forecastList && forecastList.length > 0 && forecastList
+        .filter((item, index, self) => self.findIndex((i) => new Date(i.dt * 1000).toDateString() === new Date(item.dt * 1000).toDateString()) === index)
+        .map((item) => (
+            <li key={item.dt}>
+            {new Date(item.dt * 1000).toLocaleDateString()} <span>{item.main.humidity}% {Math.floor(item.main.temp_max)}&deg; {Math.floor(item.main.temp_min)}&deg;</span>
+            </li>
+        ))}
+            </ul>
+        </div>
     )
 }
 
